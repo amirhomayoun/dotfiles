557baf958a22e9eebc6f453e2ef643b402121eb9
Amir H. Sadoughi <homayoun26@gmail.com>
2013-04-18 01:45:31 +0000

    modified zshrc, zinitrc, xdefaults and xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -21,6 +21,7 @@ myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "evince" --> doShift "test"
     , className =? "Vncviewer" --> doFloat
+    , className =? "xcalc" --> doFloat
     ]
 
 myStartUpHook = setWMName "LG3D"
@@ -88,79 +89,85 @@ main = do
                         }
 
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , handleEventHook    = fullscreenEventHook
 	, workspaces = myWorkspaces
 	, terminal = myTerminal
 	, focusedBorderColor = myFocusedBorderColor
-	, normalBorderColor = myNormalBorderColor
+    , normalBorderColor = myNormalBorderColor
 
-	} `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((mod4Mask .|. shiftMask, xK_F4), spawn "xscreensaver-command -lock; sleep 1; xset dpms force standby")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
---        , ((mod4Mask.|. shiftMask,xK_z),    spawn "gnome-screensaver-command -l")
-        , ((mod4Mask,               xK_a), sendMessage MirrorShrink)
-        , ((mod4Mask,               xK_z), sendMessage MirrorExpand)
-        
---        , (( mod4Mask, xK_c), spawn "google-chrome") -- to open Google chrome 
---        , (( mod4Mask .|. shiftMask, xK_F4), spawn "sudo shutdown -h now") -- window key + Shift + F4 to shutdown system  
-        , (( mod4Mask, xK_F4), kill) -- to kill applications  
-        , ((0  , 0x1008FF11), spawn "amixer -q set Master 5%- unmute") --Control volume 
-        , ((0  , 0x1008FF13), spawn "amixer -q set Master 5%+ unmute") --Control volume 
-        , ((0  , 0x1008FF12), spawn "amixer -q set Master toggle") --Control volume 
-        , ((0  , 0x1008FF14), spawn " mocp --toggle-pause") --
-        , ((0  , 0x1008FF15), spawn " mocp --stop") --
-        , ((0  , 0x1008FF16), spawn " mocp --previous") --
-        , ((0  , 0x1008FF17), spawn " mocp --next") --
+        } `additionalKeys`
+[ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+, ((mod4Mask .|. shiftMask, xK_F4), spawn "xscreensaver-command -lock; sleep 1; xset dpms force standby")
+, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+, ((0, xK_Print), spawn "scrot")
+    --        , ((mod4Mask.|. shiftMask,xK_z),    spawn "gnome-screensaver-command -l")
+    , ((mod4Mask,               xK_a), sendMessage MirrorShrink)
+, ((mod4Mask,               xK_z), sendMessage MirrorExpand)
 
-	, ((0, xK_Print), spawn "scrot") -- use the print key to capture screenshot with scrot  
---      , ((mod4Mask,               xK_Down),  nextWS)
---	, ((mod4Mask,               xK_Up),    prevWS)
---	, ((mod4Mask .|. shiftMask, xK_Down),  shiftToNext)
---	, ((mod4Mask .|. shiftMask, xK_Up),    shiftToPrev)
---	, ((mod4Mask,               xK_Right), nextScreen)
---	, ((mod4Mask,               xK_Left),  prevScreen)
-	, ((mod4Mask .|. shiftMask, xK_Right), shiftNextScreen)
-	, ((mod4Mask .|. shiftMask, xK_Left), shiftPrevScreen)
-	, ((mod4Mask,               xK_z),     toggleWS)
-        , ((mod4Mask     , xK_f), moveTo Next EmptyWS)  -- find a free workspace
-        , ((mod4Mask .|. shiftMask, xK_f), shiftTo Next EmptyWS)  -- shift to the next free workspace
---        , ((mod4Mask, xK_g), goToSelected defaultGSConfig)
-        , ((mod4Mask, xK_s), spawnSelected defaultGSConfig ["xterm","gmplayer","gvim"])
-        , ((mod4Mask, xK_a), spawn "/home/homayoun/.xmonad/ChooseWindow.sh" ) -- use mod4Mask + a to see all the windows in dmenu  
-        , ((mod4Mask, xK_r), spawn "/home/homayoun/.xmonad/FindFiles.sh") -- use mod4Mask + r to see a list of all files of Home directory in dmenu 
-        , ((mod4Mask .|. shiftMask, xK_r), spawn "/home/homayoun/.xmonad/FindDirectories.sh") -- use mod4Mask + shift + r to see a list of all directories in the Home directory in dmenu 
-        , ((mod4Mask, xK_g     ), windowPromptGoto  defaultXPConfig)
-        , ((mod4Mask .|. shiftMask, xK_b     ), windowPromptBring defaultXPConfig)
-	, ((mod4Mask, xK_b     ), sendMessage ToggleStruts)
-        , ((mod4Mask              , xK_y), focusUrgent )
-        , ((mod4Mask, xK_b     ), sendMessage ToggleStruts)
-         -- Increment the number of windows in the master area
-        , ((mod4Mask .|. shiftMask , xK_comma ), sendMessage (IncMasterN 1))
-         -- Deincrement the number of windows in the master area
-        , ((mod4Mask .|. shiftMask , xK_period), sendMessage (IncMasterN (-1)))
-	-- start a pomodoro
-	, ((mod4Mask              , xK_n     ), spawn "touch ~/.pomodoro_session")
-	-- Run google calendar
---	, ((mod4Mask,xK_c), spawn "chromium-browser --app='https://www.google.com/calendar/b/1/render'")
-	, ((mod4Mask,xK_c), spawn "gnome-calculator")
---        , ((mod4Mask .|. controlMask, xK_Right),        -- a crazy keybinding!
---         do t <- findWorkspace getSortByXineramaRule Next NonEmptyWS 2
---            windows . view $ t                                         )
---      ,((mod4Mask, xk_y), spawn "home/user/scripts/somescript.sh" ) -- use mod4Mask + y to run a script  
-        
+    --        , (( mod4Mask, xK_c), spawn "google-chrome") -- to open Google chrome 
+    --        , (( mod4Mask .|. shiftMask, xK_F4), spawn "sudo shutdown -h now") -- window key + Shift + F4 to shutdown system  
+    , (( mod4Mask, xK_F4), kill) -- to kill applications  
+    , ((0  , 0x1008FF11), spawn "amixer -q set Master 5%- unmute") --Control volume 
+    , ((0  , 0x1008FF13), spawn "amixer -q set Master 5%+ unmute") --Control volume 
+    --        , ((0  , 0x1008FF12), spawn "amixer -q set Master toggle") --Control volume 
+    , ((0  , 0x1008FF12), spawn "~/.xmonad/ToggleMute.sh") --Control volume 
+    --        , ((0  , 0x1008FF12), spawn "pactl set-sink-mute @DEFAULT_SINK@ 1") --Control volume 
+    --
+    --        , ((0  , 0x1008FF14), spawn " mocp --toggle-pause") --
+    --        , ((0  , 0x1008FF15), spawn " mocp --stop") --
+    --        , ((0  , 0x1008FF16), spawn " mocp --previous") --
+    --        , ((0  , 0x1008FF17), spawn " mocp --next") --
+    , ((0  , 0x1008FF14), spawn "ncmpcpp toggle;notify-send play/pause")
+    , ((0  , 0x1008FF18), spawn "ncmpcpp stop; notify-send stop")
+    , ((0  , 0x1008FF16), spawn "ncmpcpp prev;notify-send Previous ")
+    , ((0  , 0x1008FF17), spawn "ncmpcpp next")
+    , ((0, xK_Print), spawn "scrot") -- use the print key to capture screenshot with scrot  
+    --      , ((mod4Mask,               xK_Down),  nextWS)
+    --	, ((mod4Mask,               xK_Up),    prevWS)
+    --	, ((mod4Mask .|. shiftMask, xK_Down),  shiftToNext)
+    --	, ((mod4Mask .|. shiftMask, xK_Up),    shiftToPrev)
+    --	, ((mod4Mask,               xK_Right), nextScreen)
+    --	, ((mod4Mask,               xK_Left),  prevScreen)
+    , ((mod4Mask .|. shiftMask, xK_Right), shiftNextScreen)
+    , ((mod4Mask .|. shiftMask, xK_Left), shiftPrevScreen)
+, ((mod4Mask,               xK_z),     toggleWS)
+    , ((mod4Mask     , xK_f), moveTo Next EmptyWS)  -- find a free workspace
+    , ((mod4Mask .|. shiftMask, xK_f), shiftTo Next EmptyWS)  -- shift to the next free workspace
+--      , ((mod4Mask, xK_g), goToSelected defaultGSConfig)
+    --      , ((mod4Mask, xK_s), spawnSelected defaultGSConfig ["xterm","gmplayer","gvim"])
+    , ((mod4Mask, xK_a), spawn "/home/homayoun/.xmonad/ChooseWindow.sh" ) -- use mod4Mask + a to see all the windows in dmenu  
+    , ((mod4Mask, xK_r), spawn "/home/homayoun/.xmonad/FindFiles.sh") -- use mod4Mask + r to see a list of all files of Home directory in dmenu 
+    , ((mod4Mask .|. shiftMask, xK_r), spawn "/home/homayoun/.xmonad/FindDirectories.sh") -- use mod4Mask + shift + r to see a list of all directories in the Home directory in dmenu 
+    , ((mod4Mask, xK_g     ), windowPromptGoto  defaultXPConfig)
+    , ((mod4Mask .|. shiftMask, xK_b     ), windowPromptBring defaultXPConfig)
+    , ((mod4Mask, xK_b     ), sendMessage ToggleStruts)
+    , ((mod4Mask              , xK_y), focusUrgent )
+, ((mod4Mask, xK_b     ), sendMessage ToggleStruts)
+    -- Increment the number of windows in the master area
+, ((mod4Mask .|. shiftMask , xK_comma ), sendMessage (IncMasterN 1))
+    -- Deincrement the number of windows in the master area
+, ((mod4Mask .|. shiftMask , xK_period), sendMessage (IncMasterN (-1)))
+    -- start a pomodoro
+    , ((mod4Mask              , xK_n     ), spawn "touch ~/.pomodoro_session")
+    -- Run google calendar
+    , ((mod4Mask,xK_c), spawn "xcalc")
+    --        , ((mod4Mask .|. controlMask, xK_Right),        -- a crazy keybinding!
+            --         do t <- findWorkspace getSortByXineramaRule Next NonEmptyWS 2
+            --            windows . view $ t                                         )
+    --      ,((mod4Mask, xk_y), spawn "home/user/scripts/somescript.sh" ) -- use mod4Mask + y to run a script  
+
 
     -- volume control.
---        , ("M-xK_F6", raiseVolume 4 >> return ())
---        , ((mod4Mask,xK_F6), spawn "amixer -q set Master 5%- unmute") 
---        , ((mod4Mask,xK_F7), spawn "amixer -q set Master 5%+ unmute") 
---        , ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
---        , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%- unmute")
---        , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+ unmute")
-        ]
---main = xmonad defaultConfig { keys = keys defaultConfig `mappend`
---    \c -> fromList [
---        ((0, xK_F6), lowerVolume 4 >>= alert),
---        ((0, xK_F7), raiseVolume 4 >>= alert)
---    ]
---}
+    --        , ("M-xK_F6", raiseVolume 4 >> return ())
+    --        , ((mod4Mask,xK_F6), spawn "amixer -q set Master 5%- unmute") 
+    --        , ((mod4Mask,xK_F7), spawn "amixer -q set Master 5%+ unmute") 
+    --        , ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
+    --        , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%- unmute")
+    --        , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+ unmute")
+    ]
+    --main = xmonad defaultConfig { keys = keys defaultConfig `mappend`
+        --    \c -> fromList [
+            --        ((0, xK_F6), lowerVolume 4 >>= alert),
+            --        ((0, xK_F7), raiseVolume 4 >>= alert)
+                --    ]
+                --}
--- .zshrc
+++ .zshrc
@@ -35,7 +35,8 @@ ZSH_THEME="robbyrussell"
 plugins=(git github)
 
 source $ZSH/oh-my-zsh.sh
-source /etc/zsh_command_not_found
+#source /etc/zsh_command_not_found
+[ -r /usr/share/doc/pkgfile/command-not-found.zsh ] && . /usr/share/doc/pkgfile/command-not-found.zsh
 
 # Customize to your needs...
 export PATH=/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
@@ -50,3 +51,30 @@ alias alert='notify-send -i /usr/share/icons/gnome/32x32/apps/gnome-terminal.png
 #TERM=rxvt-unicode
 TERM=rxvt-256color
 COLORTERM='rxvt-unicode-256color'
+
+
+# key bindings
+# bindkey "\e[1~" beginning-of-line
+# bindkey "\e[4~" end-of-line
+# bindkey "\e[5~" beginning-of-history
+# bindkey "\e[6~" end-of-history
+# bindkey "\e[3~" delete-char
+# bindkey "\e[2~" quoted-insert
+# bindkey "\e[5C" forward-word
+# bindkey "\eOc" emacs-forward-word
+# bindkey "\e[5D" backward-word
+# bindkey "\eOd" emacs-backward-word
+# bindkey "\ee[C" forward-word
+# bindkey "\ee[D" backward-word
+# bindkey "^H" backward-delete-word
+# # for rxvt
+# bindkey "\e[8~" end-of-line
+# bindkey "\e[7~" beginning-of-line
+# # for non RH/Debian xterm, can't hurt for RH/DEbian xterm
+# bindkey "\eOH" beginning-of-line
+# bindkey "\eOF" end-of-line
+# # for freebsd console
+# bindkey "\e[H" beginning-of-line
+# bindkey "\e[F" end-of-line
+# # completion in the middle of a line
+# bindkey '^i' expand-or-complete-prefix
